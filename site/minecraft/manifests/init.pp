c70710f492c873d2cb3cfa1b9fd13044980a7618
Jackson Montijo <55205211+jhmontijo@users.noreply.github.com>
2020-09-16 13:50:03 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,18 +4,18 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar'
+    source => 'https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar',
   }
   package {'java':
     ensure => present,
   }
   file{'/opt/minecraft/eula.txt':
     ensure  => present,
-    content => 'eula=true'
+    content => 'eula=true',
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service'
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
     ensure => running,
